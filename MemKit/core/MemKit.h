ed28458b3a99459fbabf166d961a8f27a0ff4a51
Hu Jian <1425124481@qq.com>
2016-09-12 21:34:52 +0800

    update the apis

--- MemKit/core/MemKit.h
+++ MemKit/core/MemKit.h
@@ -1,5 +1,5 @@
 /**
- *      copyright C hujian 2016 version 1.0.0
+ *      CopyRight C hujian 2016 version 1.0.0
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
